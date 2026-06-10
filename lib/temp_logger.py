b022b4c005a947b4a51e007f8a88adf665ed6bf0
Liam <liam.fitz85@protonmail.com>
2026-06-09 20:48:36 -0700

    using incorrect function signature.

--- lib/temp_logger.py
+++ lib/temp_logger.py
@@ -30,7 +30,7 @@ class TempLogger:
                 f"{str(self.__dht.get_temp_c())}*C,\n"
                 f"{str(self.to_fahrenheit(self.__dht.get_temp_c()))}*F,\n"
                 f"{str(self.__dht.get_humidity())}%\n"
-                f"{self.__wifi.get_date()}"
+                f"{self.__wifi.get_time()}"
         )
 
     def to_fahrenheit(self, celsius):
@@ -43,7 +43,7 @@ class TempLogger:
             'celsius': self.__dht.get_temp_c(),
             'fahrenheit': self.to_fahrenheit(self.__dht.get_temp_c()),
             'humidity': self.__dht.get_humidity(),
-            'date': self.__wifi.get_date()
+            'date': self.__wifi.get_time()
         })
 
     def to_csv(self):
@@ -53,7 +53,7 @@ class TempLogger:
                 temp_c = self.__dht.get_temp_c()
                 temp_f = self.to_fahrenheit(temp_c)
                 humidity = self.__dht.get_humidity()
-                current_time = self.__wifi.get_date()
+                current_time = self.__wifi.get_time()
                 outfile.write(
                     f"{self.__location},"
                     f"{temp_c},{temp_f},"
